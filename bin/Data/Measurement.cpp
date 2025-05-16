c3e273d554e84662217e79785698be5bf630457d
nosense1234 <you@example.com>
2025-05-16 14:48:47 +0200

    Changed Measurement.cpp and .h

--- bin/Data/Measurement.cpp
+++ bin/Data/Measurement.cpp
@@ -29,11 +29,100 @@ bool Measurement::isWithin(time_t period_start, time_t period_end)
 //} //----- Fin de isWithin
 
 
+
+
+void Measurement::setTs(time_t time)
+{
+    ts = time;
+    
+}
+// Algorithme :
+//
+//{
+//} //----- Fin de isWithin
+
+void Measurement::setAttr_desc(string desc)
+{
+    attr_desc = desc;
+}   
+// Algorithme :
+//
+//{
+//} //----- Fin de isWithin
+
+void Measurement::setAttr_unit(string unit)
+{
+    attr_unit = unit;
+}
+   
+// Algorithme :
+//
+//{
+//} //----- Fin de isWithin
+
+void Measurement::setValue(double val)
+{
+    value = val;
+}
+  
+// Algorithme :
+//
+//{
+//} //----- Fin de isWithin
+
+time_t Measurement::getTs()
+{
+    return ts;
+}
+   
+// Algorithme :
+//
+//{
+//} //----- Fin de isWithin
+   
+string Measurement::getAttr_desc()
+{
+    return attr_desc;
+}
+    
+// Algorithme :
+//
+//{
+//} //----- Fin de isWithin
+   
+string Measurement::getAttr_unit()
+{
+    return attr_unit;
+}
+    
+// Algorithme :
+//
+//{
+//} //----- Fin de isWithin
+
+double Measurement::getValue()
+{
+    return value;
+}    
+// Algorithme :
+//
+//{
+//} //----- Fin de isWithin
+
+
+
+
+
+
 //------------------------------------------------- Surcharge d'opérateurs
 Measurement& Measurement::operator = ( const Measurement& unMeasurement )
 // Algorithme :
 //
 {
+
+    Measurement mesurement  = Measurement(unMeasurement);
+    return mesurement;
+
 } //----- Fin de operator =
 
 
@@ -45,16 +134,26 @@ Measurement::Measurement ( const Measurement & unMeasurement )
 #ifdef MAP
     cout << "Appel au constructeur de copie de <Measurement>" << endl;
 #endif
+
+    ts = unMeasurement.ts;
+    attr_desc = unMeasurement.attr_desc; 
+    attr_unit = unMeasurement.attr_unit;
+    value = unMeasurement.value;
 } //----- Fin de Measurement (constructeur de copie)
 
 
-Measurement::Measurement ( )
+Measurement::Measurement ( time_t time, string unit, string desc, double val )
 // Algorithme :
 //
 {
 #ifdef MAP
     cout << "Appel au constructeur de <Measurement>" << endl;
 #endif
+
+    ts = time;
+    attr_desc = unit ;
+    attr_unit = desc;
+    value = val;
 } //----- Fin de Measurement
 
 
